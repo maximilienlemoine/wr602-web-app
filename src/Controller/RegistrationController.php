89edd4c77297bb94dc96a8dff1456faf25a517a9
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-05-22 14:33:36 +0200

    fix : phpmd

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -24,8 +24,12 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
-    {
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        Security $security,
+        EntityManagerInterface $entityManager
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
